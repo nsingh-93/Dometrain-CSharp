23f8fde96ac86c3b9b41a6baef00e189cd5eba1d
Neil <22299102+nsingh-93@users.noreply.github.com>
2025-02-16 20:48:57 -0500

    Finished advanced methods module

---
+++ deep-dive/AdvancedMethods/Events.cs
@@ -0,0 +1,67 @@
+namespace AdvancedMethods;
+
+// Events are more used in UI applications
+public class Events
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public MessageEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+
+    public class EventSource
+    {
+        public event EventHandler<MessageEventArgs> SourceChanged;
+
+        public void RaiseEvent(string message)
+        {
+            SourceChanged?.Invoke(this, new MessageEventArgs(message));
+        }
+    }
+
+    public static void RunEvents()
+    {
+        EventSource source = new EventSource();
+
+        // Add new handler
+        source.SourceChanged += Source_SourceChanged;
+
+        // Raise the event
+        source.RaiseEvent("Hello world");
+
+        // Remove the event
+        source.SourceChanged -= Source_SourceChanged;
+
+
+        // Can chain multiple handlers
+        source.SourceChanged += Source_SourceChanged1;
+        source.SourceChanged += Source_SourceChanged2;
+
+        source.RaiseEvent("Hello world from Neil");
+
+
+        void Source_SourceChanged(object? sender, MessageEventArgs e)
+        {
+            Console.WriteLine($"Sender: { sender }");
+            Console.WriteLine($"Message: { e.Message }");
+        }
+
+        void Source_SourceChanged1(object? sender, MessageEventArgs e)
+        {
+            Console.WriteLine("First handler");
+            Console.WriteLine($"Sender: { sender }");
+            Console.WriteLine($"Message: { e.Message }");
+        }
+
+        void Source_SourceChanged2(object? sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Second handler");
+            Console.WriteLine($"Sender: { sender }");
+            Console.WriteLine($"Message: { e.Message }");
+        }
+    }
+}
--- deep-dive/AdvancedMethods/Laziness.cs
+++ deep-dive/AdvancedMethods/Laziness.cs
@@ -1,8 +1,40 @@
 namespace AdvancedMethods;
 
+// Lazy class gives us singleton like behavior not visible gloabally
 public class Laziness
 {
     public static void RunLaziness()
     {
+        Lazy<int> lazyValue = new(() =>
+        {
+            Console.WriteLine("This will run only once.");
+            Console.WriteLine("Finding max value");
+
+            int[] numbers = [35, 20, 30, 40, 50];
+
+            int max = int.MinValue;
+
+            foreach (var num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                // Just to make this seem expensive
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"The max value is { max }");
+
+            return max;
+        });
+
+        Console.WriteLine($"The value of lazyValue is { lazyValue.Value }");
+        // Computes once and the others are already stored - This also makes it thread safe
+        Console.WriteLine($"The value of lazyValue is { lazyValue.Value }");
+        Console.WriteLine($"The value of lazyValue is { lazyValue.Value }");
+        Console.WriteLine($"The value of lazyValue is { lazyValue.Value }");
+        Console.WriteLine($"The value of lazyValue is { lazyValue.Value }");
     }
 }
